248dcefecc02f467ed066df2ba575cbe24b5b05e
igot2bfreee <igot2bfreee@gmail.com>
2020-07-16 01:27:13 -0700

    fiik

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ $(function() {
     $("#loginModal").modal();
     });
 });
+
+$(function() {
+  $('.arrow').click(function() {
+    $('html').animate({ scrollTop: $('html').prop('scrollHeight')}, 1200);
+  });
+});
+
