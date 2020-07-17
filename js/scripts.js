a8514dec6ce61fec5ffd300ce1cc069b07fd6042
igot2bfreee <igot2bfreee@gmail.com>
2020-07-16 19:58:12 -0700

    almost done

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,14 @@ $(function() {
   });
 });
 
+$(function() {
+  $(window).on('scroll', function() {
+
+    if ($(window).scrollTop() + $(window).height() > $('.wrapper').outerHeight()) {
+      $('.arrow').hide();
+    } else {
+      $('.arrow').show();
+    }
+  });
+});
+
